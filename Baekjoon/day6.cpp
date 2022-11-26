0ed8e80c624153975484ec1b10a528f2e05093c8
LuBly <lumenize@ajou.ac.kr>
2022-11-26 13:28:24 +0900

    [백준 c++] 1929 소수 구하기

---
+++ Baekjoon/day6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	int first, last;
+	cin >> first >> last;
+	vector<bool> isPrime(last + 1, true);
+	isPrime[0] = false;
+	isPrime[1] = false;
+
+	for (int k = 2; k*k <= last; k++) {
+		if (isPrime[k]) {
+			for (int j = k * k; j <= last; j+=k) {
+				isPrime[j] = false;
+			}
+		}
+	}
+	for (int a = first; a <= last; a++) {
+		if (isPrime[a]) printf("%d\n",a);
+	}
+}
