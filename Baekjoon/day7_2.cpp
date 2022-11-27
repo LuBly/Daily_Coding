6d80929606c37f69aaa380ed13ae62d19a6b12bc
LuBly <lumenize@ajou.ac.kr>
2022-11-27 19:59:28 +0900

    [Programmers C++] 과일 장수

---
+++ Baekjoon/day7_2.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    int cur = 0;
+    sort(score.rbegin(),score.rend());
+    
+    for(int k =0; k<score.size()/m; k++){
+        answer += (score[cur+m-1]*m);
+        cur+=m;
+    }
+   
+    return answer;
+}
