f32e4f9dade0a0b66321ee5091742dc27692efd6
LuBly <lumenize@ajou.ac.kr>
2022-11-30 17:07:27 +0900

    [Programmers C++] 연속 부분 수열 합의 개수

---
+++ Baekjoon/day9.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> elements) {
+    int answer = 0;
+    int cur = 0;
+    int leng = 1;
+    vector<int> tmp;
+    
+    while(leng<=elements.size()){
+        if(leng!=elements.size()){
+            for(int start = 0; start < elements.size(); start++){
+                int sum = 0;
+                for(int k = 0; k < leng; k++){
+                    sum += elements[(start+k)%elements.size()];
+                }
+                tmp.push_back(sum);
+            }
+        }
+        else{
+            int F_sum = 0;
+            for(int j = 0; j < leng; j++){
+                F_sum += elements[j];
+            }
+            tmp.push_back(F_sum);
+        }
+        leng++;
+    }
+    sort(tmp.begin(),tmp.end());
+    tmp.erase(unique(tmp.begin(),tmp.end()),tmp.end());
+    answer = tmp.size();
+    
+    return answer;
+}
