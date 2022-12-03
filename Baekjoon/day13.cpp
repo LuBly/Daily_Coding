f52d8816a220ce827ddb41ab74cb850bdccf4c87
LuBly <lumenize@ajou.ac.kr>
2022-12-03 20:23:48 +0900

    최솟값 만들기

---
+++ Baekjoon/day13.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+
+    sort(A.begin(),A.end());
+    sort(B.rbegin(),B.rend());
+    
+    for(int k = 0; k<A.size(); k++){
+        answer += A[k]*B[k];
+    }
+
+    return answer;
+}
