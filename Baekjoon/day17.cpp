94b82fbc86d8da4e4dd9c233f9a3d706078eb8f1
LuBly <lumenize@ajou.ac.kr>
2022-12-08 21:52:14 +0900

    [Programmers C++] 올바른 괄호

---
+++ Baekjoon/day17.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    vector<bool> is_Open;
+    for(int k = 0; k<s.size(); k++){
+        if(s[k]=='('){
+            is_Open.push_back(true);
+        }
+        else{
+            if(is_Open.empty()){
+                answer = false;
+                break;
+            }
+            else is_Open.pop_back();
+        }
+    }
+    if(!is_Open.empty()) answer = false;
+    return answer;
+}
