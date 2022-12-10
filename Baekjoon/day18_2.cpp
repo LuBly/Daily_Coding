2ace92492bede35c45ac4aaece3c6344324f7dc6
LuBly <lumenize@ajou.ac.kr>
2022-12-10 20:04:25 +0900

    [Programmers C++] 숫자의 표현

---
+++ Baekjoon/day18_2.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int count=1;
+    while((count-1)*count/2 < n){
+        if((n-(count-1)*count/2)%count==0) answer++;
+        count++;
+    }
+    return answer;
+}
