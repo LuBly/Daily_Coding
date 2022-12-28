b1dd67b1eccb6d430144d4425a75ce74956004ca
LuBly <lumenize@ajou.ac.kr>
2022-12-28 23:21:21 +0900

    [Programmers C++] 수박수박수박수박수박수?

---
+++ Study/day33_4.cpp
@@ -0,0 +1,14 @@
+//[Programmers C++] 수박수박수박수박수박수?
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for(int k=0;k<n;k++){
+        if(k%2==0) answer+="수";
+        else answer+="박";
+    }
+    return answer;
+}
