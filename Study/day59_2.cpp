bd174af361e4c30b055c905fcd7c08f9b8f31ca8
LuBly <lumenize@ajou.ac.kr>
2023-02-07 15:51:44 +0900

    [BaekJoon C++] N과 M (2)

---
+++ Study/day59_2.cpp
@@ -0,0 +1,22 @@
+//[BaekJoon C++] N과 M(2) [백트래킹] 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int num, pick;
+    vector<int> perm;
+    cin>>num>>pick;
+
+    for(int k=1;k<=num;k++) perm.push_back(k);
+
+    do{
+        if(is_sorted(perm.begin(),perm.begin()+pick)){
+            for(int j=0;j<pick;j++) cout<<perm[j]<<" ";
+            cout<<"\n";    
+        }
+        reverse(perm.begin()+pick,perm.end());
+    } while(next_permutation(perm.begin(),perm.end()));
+}
