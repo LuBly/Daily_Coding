81a5783e32aff034284174a4e682668dc28f4f2b
LuBly <lumenize@ajou.ac.kr>
2023-02-24 13:14:05 +0900

    [Baekjoon C++] 인간-컴퓨터 상호작용

---
+++ Study/day77.cpp
@@ -0,0 +1,34 @@
+//[Baekjoon C++] 인간-컴퓨터 상호작용
+#include <iostream>
+using namespace std;
+
+int q, pSum[200001][26];
+string S;
+
+int main() {ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+    cin >> S;
+    for (int i = 0; i < S.length(); i++) {
+	    // 이전꺼 합
+	    if (i != 0) {
+		    for (int k = 0; k < 26; k++)
+			    pSum[i][k] = pSum[i - 1][k];
+    	}
+	    int now = S[i] - 'a';
+	    pSum[i][now]++;
+    }
+
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        char chk;
+        int l, r;
+        cin >> chk >> l >> r;
+        int now = chk - 'a';
+
+        int p1 = l > 0 ? pSum[l - 1][now] : 0;
+        int p2 = pSum[r][now];
+        cout << p2 - p1 << '\n';
+    }
+
+    return 0;
+}
