9163e6d69abf30f0747366b6c24d0bff1cbe2ca7
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2023-03-21 13:57:53 +0900

    [unrated] Title: 덧칠하기, Time: 0.23 ms, Memory: 5.02 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/README.md
+++ 프로그래머스/unrated/161989. 덧칠하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 덧칠하기 - 161989 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989) 
 
 ### 성능 요약
 
-메모리: 5.04 MB, 시간: 3.05 ms
+메모리: 5.02 MB, 시간: 0.23 ms
 
 ### 구분
 
--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -1,24 +1,20 @@
 #include <string>
 #include <vector>
-#include <iostream>
-using namespace std;
 
+using namespace std;
+//erase를 사용하지 않는 빠른 풀이법
 int solution(int n, int m, vector<int> section) {
-    int answer = 0;
-    
-    while(section.size()>0){//section에 남은 값이 없을때 까지 계속해서 반복을 수행
-        int cur=section[0];
-        int limit=cur+m-1;
-        int idx=0;//erase를 활용하기 위해 section을 순회하면서 limit보다 작다는 조건을 만족할 경우 idx를 하나씩 증가시킴 
-        
-        for(int k=0;k<section.size();k++){//section 벡터를 순회하면서 limit 값보다 작은 부분을 삭제
-            if(section[k]<=limit)     
-                idx++;
+    int answer = 1;
+
+    int pivot = section[0];
+    for(const auto v : section){//section을 순회
+        if(v < pivot + m)//section의 값이 pivot+m(페인트를 칠하는 범위)보다 작으면 continue
+            continue;
+        else{//section의 값이 pivot+m보다 크면 한번 더 칠해야 하므로 해당 위치를 pivot으로 설정하고 answer++;
+            pivot = v;
+            answer++;
         }
-        section.erase(section.begin(),section.begin()+idx);//시작부터 증가된 idx까지 section을 erase하는 작업 수행
-        //한번 붓질을 통해 칠할 수 있는 벽면을 section에서 삭제함
-        answer++;
     }
-    
+
     return answer;
 }
