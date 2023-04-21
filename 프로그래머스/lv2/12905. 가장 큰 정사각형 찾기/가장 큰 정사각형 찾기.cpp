0fe12774186c38b028c73897e23e413fbaa78e5a
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2023-04-21 22:58:59 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 4.88 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -1,18 +1,21 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
 int solution(vector<vector<int>> board)
 {
-    int answer = board[0][0];
-    int width=board[0].size(), length=board.size();
-    for(int k=1; k<length; k++){
-        for(int j=1; j<width; j++){
-            if(board[k][j]==1){//방문한 위치가 1일 경우, 해당 위치를 기점으로 위, 왼쪽, 왼 대각 위가 모두 같다면 정사각형이 구성되고, 아니라면 숫자가 낮아지므로 체크
-                board[k][j]+=min({board[k-1][j],board[k][j-1],board[k-1][j-1]});
-                answer = max(board[k][j],answer);
+    int answer = 0;
+    for(int y=1;y<board.size();y++){
+        for(int x=1;x<board[0].size();x++){
+            if(board[y][x]>0){//해당 지점이 1일 경우
+                board[y][x] = min({board[y-1][x],board[y][x-1],board[y-1][x-1]})+1;
             }
+        }
+    }
+    
+    for(int y=0;y<board.size();y++){
+        for(int x=0;x<board[0].size();x++){
+            answer = (board[y][x]>answer) ? board[y][x] : answer;
         }
     }
     
