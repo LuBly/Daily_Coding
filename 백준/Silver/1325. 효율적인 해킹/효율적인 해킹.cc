04a982dba07e39d799d49b23f43aa2bd9546b8bb
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2023-07-25 19:01:34 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2628 ms, Memory: 5388 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,27 +1,29 @@
-//[Baekjoon C++] 효율적인 해킹 [BFS]
+//[Baekjoon C++] 효율적인 해킹 [bfs]
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <queue>
-#define MAX 10001
+#define MAX 100001
 using namespace std;
 
 vector<int> board[MAX];
-bool is_visit[MAX] = {false,};
+bool isVisited[MAX]={false,};
 
-int BFS(int start, int cnt){
-    is_visit[start]=true;
-    queue<int> q;
+int bfs(int start){
+    queue<int>q;
     q.push(start);
-
+    isVisited[start]=true;
+    int cnt = 0;
     while(!q.empty()){
         int cur = q.front();
         q.pop();
-        //현재 노드에서 해킹 가능한 pc 전부 탐색
-        for(int idx=0;idx<board[cur].size();idx++){
-            int nx = board[cur][idx];
-            if(!is_visit[nx]){
-                is_visit[nx]=true;
+        //cur을 기준으로 접근 가능한 node 카운트
+        for(int k=0;k<board[cur].size();k++){
+            int nx = board[cur][k];
+            //방문한적 없는 노드라면 다음 q에 입력
+            if(!isVisited[nx]){
                 q.push(nx);
+                isVisited[nx]=true;
                 cnt++;
             }
         }
@@ -29,41 +31,35 @@ int BFS(int start, int cnt){
     return cnt;
 }
 
-void resetVisit(int N){
-    for(int k=1;k<=N;k++)
-        is_visit[k]=false;
+void reset(int nodeCnt){
+    for(int k=1;k<=nodeCnt;k++){
+        isVisited[k]=false;
+    }
 }
 
 int main() {ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-    int N,M;
-    cin>>N>>M;
-    //board 생성
-    for(int k=0;k<M;k++){
-        int from,to;
-        cin>>from>>to;
-        //A(from)->B(to)를 신뢰하면 B(to)=>A(from)로 해킹 가능
-        board[to].push_back(from);
+    int node, edge;
+    cin>>node>>edge;
+    for(int k=0;k<edge;k++){
+        int a,b;
+        cin>>a>>b;
+        //a->b를 신뢰 = b를 타고 a까지 갈 수 있다.
+        board[b].push_back(a);
     }
 
-    int result[MAX];//결과 저장(node/연결가능 컴퓨터의 개수)
-    //노드를 하나하나 순회하며 BFS 계산
-    for(int k=1;k<=N;k++){
-        int count = BFS(k,1);
-        resetVisit(N);
-        result[k]=count;
-    }
+    //첫번째 노드부터 총 방문 가능한 컴퓨터의 갯수 저장
+    vector<int> canHack(node+1,0);
+    //최대로 해킹 가능한 컴퓨터의 수를 체크하기 위한 변수
+    int maxHack=0;
 
-    //최대 해킹 가능한 수 계산
-    int maxCnt=-1;
-    for(int k=1;k<=N;k++){
-        if(result[k]>maxCnt)
-            maxCnt = result[k];
+    for(int k=1;k<=node;k++){
+        int result = bfs(k);
+        canHack[k]=result;
+        maxHack = max(maxHack,result);
+        reset(node);
     }
     
-    //오름차순 출력
-    for(int k=1;k<=N;k++){
-        if(result[k]==maxCnt)
-            cout<<k<<" ";
+    for(int k=1;k<=node;k++){
+        if(canHack[k]==maxHack) cout<<k<<" ";
     }
-    
 }
