34463ff9bb9c4fe664f7938e677165a163011448
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2023-08-04 10:20:02 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 52 ms, Memory: 5732 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -2,64 +2,66 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <limits.h>
+#include <algorithm>
 #define MAX 987654321
 using namespace std;
 
-//다익스트라 세번
-//1번 노드->v1->v2->N
-vector<pair<int,int>> board[801];//node, cost
+vector<vector<pair<int,int>>> board(801);//시작지점 / 도착지점,cost
 vector<int> cost(801);
-int node, edge;
+int nodeCount, edgeCount;
 
 void dijkstra(int start){
-    for(int k=0;k<=node;k++) cost[k]=MAX;
-    priority_queue<pair<int,int>> pq;//cost, node
+    for(int k = 0; k <= nodeCount; k++) cost[k] = MAX;
+    priority_queue<pair<int,int>> pq; //cost, node
     cost[start]=0;
     pq.push({0,start});
     while(!pq.empty()){
-        int cur_node = pq.top().second;
-        int cur_cost = -pq.top().first;
+        int curNode = pq.top().second;
+        int curCost = -pq.top().first;
         pq.pop();
-        
-        for(int k=0;k<board[cur_node].size();k++){
-            int next_node = board[cur_node][k].first;
-            int next_cost = cur_cost+board[cur_node][k].second;
-            if(next_cost<cost[next_node]){//기존 비용보다 지금 경로로 갔을 때 비용이 낮다면 갱신
-                cost[next_node]=next_cost;
-                pq.push({-next_cost,next_node});
+
+        for(int k = 0; k < board[curNode].size(); k++){
+            int nextNode = board[curNode][k].first;
+            int nextCost = curCost + board[curNode][k].second;
+            //dest까지 도달하는데 현재 저장된 cost보다 현재 방문한 노드를 거치는 cost가 더 적다면 
+            //cost 변경
+            if(nextCost < cost[nextNode]){
+                cost[nextNode] = nextCost;
+                pq.push({-nextCost, nextNode});
             }
         }
     }
 }
 
 int main() {ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-    cin>>node>>edge;
-    //board data 입력
-    for(int k=0;k<edge;k++){
-        int st, dt, cost;
-        cin>>st>>dt>>cost;
-        board[st].push_back({dt,cost});
-        board[dt].push_back({st,cost});
+    cin>>nodeCount>>edgeCount;
+
+    for(int k = 0; k < edgeCount; k++){
+        int start, dest, cost;
+        cin>>start>>dest>>cost;
+        board[start].push_back({dest,cost});
+        board[dest].push_back({start,cost});
     }
-    int v1,v2;
-    cin>>v1>>v2;
-    dijkstra(1);//1에서 출발해서 v1, v2에 도달할 수 있는 최단 거리
-    int s_v1 = cost[v1];
-    int s_v2 = cost[v2];
 
-    dijkstra(v1);//v1에서 출발해서 v2, N에 도달할 수 있는 최단거리
-    int v1_v2 = cost[v2];
-    int v1_N = cost[node];
+    int adj1, adj2;
+    cin>>adj1>>adj2;
 
-    dijkstra(v2);//v2에서 출발해서 N에 도달할 수 있는 최단거리
-    int v2_N = cost[node];
+    //1에서 시작해서 모든 노드에 도달하는 최단거리
+    dijkstra(1);
+    int fromStartToAdj1 = cost[adj1];
+    int fromStartToAdj2 = cost[adj2];
 
-    int result=MAX;
-    result = min(result,s_v1+v1_v2+v2_N);//s->v1->v2->N
-    result = min(result,s_v2+v1_v2+v1_N);//s->v2->v1->N 
+    dijkstra(adj1);
+    int fromAdj1ToAdj2 = cost[adj2];
+    int fromAdj1ToDest = cost[nodeCount];
 
-    if(v1_v2==MAX||result==MAX) cout<<-1;
-    else cout<<result;
+    dijkstra(adj2);
+    int fromAdj2ToDest = cost[nodeCount];
 
+    int result = MAX;
+    result = min(result, fromStartToAdj1+fromAdj1ToAdj2+fromAdj2ToDest);//s->adj1->adj2->N
+    result = min(result, fromStartToAdj2+fromAdj1ToAdj2+fromAdj1ToDest);//s->adj2->adj1->N
+
+    if(fromAdj1ToAdj2 == MAX || result == MAX) cout << -1;
+    else cout << result;
 }
