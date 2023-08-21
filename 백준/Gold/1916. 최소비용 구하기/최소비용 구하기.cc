067284620b06cccb1c2bcd5963fee53e0ce4bd2a
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2023-08-21 14:39:23 +0900

    [Gold V] Title: 최소비용 구하기, Time: 32 ms, Memory: 4748 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -1,53 +1,60 @@
-//[BaekJoon C++] 최소비용 구하기 [구현]
+//[BaekJoon C++] 최소비용 구하기 [다익스트라]
 #include <iostream>
 #include <vector>
 #include <queue>
+#define MAX 1001
 #define INF 987654321
 using namespace std;
 
-vector<int> dijkstra(int start, vector<vector<pair<int,int>>> map, vector<int> costs){
+// board : 각 노드별로 연결되어 있는 노드와 cost를 저장할 vector [from] {dist, to};
+// cost  : 출발 도시부터 idx 도시까지의 최소 거리를 저장할 vector
+vector<vector<pair<int, int>>> board(MAX);
+vector<int> cost(MAX, INF);
+
+void dijkstra(int start){
+    // pq : cost를 오름차순으로 정리해서 저장하고 있을 priority queue
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    //{cost, dest}를 저장할 priorty queue
-    //cost를 기준으로 오름차순 정렬
-    pq.push({0, start});
-    costs[start] = 0;
+    // 초기화
+    pq.push({0,start});
+    cost[start] = 0;
+
     while(!pq.empty()){
         int curCost = pq.top().first;
         int curNode = pq.top().second;
         pq.pop();
+        // 현재 들어온 cost가 이전에 계산해놓은 cost보다 클경우 continue;
+        // -> 경유지를 경유했을 때 더 짧은 길이 있는지를 확인하려는 것
+        // -> 경유지를 경유하기도 전에 cost가 더 크다면 구지 확인할 필요가 없다.
+        if(curCost > cost[curNode]) continue;
 
-        // 현재 들어온 경로가 기존 경로보다 cost가 높으면 그냥 패스
-        if(curCost > costs[curNode]) continue;
-
-        for(int idx = 0; idx < map[curNode].size(); idx++){
-            int nextCost = curCost + map[curNode][idx].second;
-            int nextNode = map[curNode][idx].first;
-            // 현재 경유 노선의 cost가 기존에 저장된 경로보다 낮은 cost일 경우 그 길로 이동
-            if(nextCost < costs[nextNode]){
-                costs[nextNode] = nextCost;
+        for(int k = 0; k < board[curNode].size(); k++){
+            int nextCost = curCost + board[curNode][k].first;
+            int nextNode = board[curNode][k].second;
+            // 신규 경로의 cost가 기존 cost 보다 작다면 신규 경로 및 cost 저장
+            if(nextCost < cost[nextNode]){
+                cost[nextNode] = nextCost;
                 pq.push({nextCost, nextNode});
             }
         }
     }
-
-    return costs;
 }
 
-
 int main(void){ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    // cityCnt : 도시의 개수 (node 수)
+    // busCnt  : 버스의 개수 (edge 수)
     int cityCnt, busCnt;
-    cin >> cityCnt >> busCnt;
+    cin >> cityCnt >> busCnt; 
 
-    vector<vector<pair<int,int>>> map(cityCnt+1);//cityCnt 만큼의 map vector 생성
-    vector<int> costs(cityCnt+1,INF);
     for(int k = 0; k < busCnt; k++){
-        int from, to, cost;
-        cin >> from >> to >> cost;
-        map[from].push_back({to, cost});
-    }
+        int from, to, dist;
+        cin >> from >> to >> dist;
+        board[from].push_back({dist, to});
+    }   
+    // st : 시작점
+    // dt : 목적지
+    int st, dt;
+    cin>> st >> dt;
+    dijkstra(st);
 
-    int start, dest;
-    cin>> start >> dest;
-    vector<int> result = dijkstra(start, map, costs);
-    cout << result[dest];
+    cout<<cost[dt];
 }   
