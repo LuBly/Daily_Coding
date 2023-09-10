a796cefb21e9defd59e42ad4d651ef906525523d
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2023-09-10 21:48:42 +0900

    [Gold V] Title: 강의실 배정, Time: 56 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.cc
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -1,23 +1,22 @@
-//[Baekjoon C++] 강의실 배정 [구현]
+//[Baekjoon C++] 강의실 배정 [그리디]
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
+#include <queue>
 using namespace std;
+
 
 int main() {ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
     int N;
-    cin>>N;
+    cin >> N;
     vector<pair<int,int>> data;
-    for(int k=0;k<N;k++){
-        int start, fin;
-        cin>>start>>fin;
-        data.push_back({start,fin});
+    for(int k = 0; k < N; k++){
+        int st, fin;
+        cin >> st >> fin;
+        data.push_back(make_pair(st, fin));
     }
-    //강의를 최대 몇개까지 들을 수 있냐 X
-    //모든 강의를 위해서 최소 몇 개의 강의실이 필요한가
+    sort(data.begin(), data.end());
 
-    sort(data.begin(),data.end());
     priority_queue<int,vector<int>,greater<int>> pq;
     for(int k=0;k<N;k++){
         //강의가 끝나는 시간들을 pq에 push
