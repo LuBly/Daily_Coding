dcf508abf10ba2cf1c4a8ba6d210275fd7752a89
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2023-11-21 10:48:23 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -4,35 +4,29 @@
 #include <algorithm>
 using namespace std;
 
-/*
-//마지막 stair을 반드시 밟는다고 가정
-dp[1] = arr[1]
-dp[2] = arr[1] + arr[2]
-dp[3] = arr[1] + arr[3] vs arr[2] + arr[3]
+int main(void){ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int cnt;
+    cin >> cnt;
+    vector<int> arr(cnt+1);
+    vector<int> dp(cnt+1);
 
-dp[4] = dp[1] + arr[3] + arr[4] , dp[2] + arr[4]
-dp[N] = dp[N-3] + arr[N-1] + arr[N] vs dp[N-2] + arr[N]
-*/
-
-
-int main() {ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);    
-    int stairCnt;
-    cin >> stairCnt;
-
-    vector<int> arr(stairCnt + 1); 
-    vector<int> dp(stairCnt + 1); 
-    for(int k = 1; k <= stairCnt; k++){
+    for(int k = 1; k <= cnt; k++){
         cin >> arr[k];
     }
-
+    /*
+    dp[1] = arr[1]
+    dp[2] = arr[1] + arr[2]
+    dp[3] = arr[2] + arr[3]
+    dp[4] = dp[2] + arr[4] or dp[1] + arr[3] + arr[4]
+    dp[5] = dp[3] + arr[5] or dp[2] + arr[4] + arr[5]
+    dp[k] = max(dp[k-2] + arr[k], dp[k-3] + arr[k-1] + arr[k])
+    */
     dp[1] = arr[1];
     dp[2] = arr[1] + arr[2];
     dp[3] = max(arr[1] + arr[3], arr[2] + arr[3]);
-
-    for(int k = 4; k <= stairCnt; k++){
-        dp[k] = max(dp[k-3] + arr[k-1] + arr[k], dp[k-2] + arr[k]);
+    
+    for(int k = 4; k <=cnt; k++){
+        dp[k] = max(dp[k-2] + arr[k], dp[k-3] + arr[k-1] + arr[k]);
     }
-
-    cout << dp[stairCnt];
-
+    cout << dp[cnt];
 }
