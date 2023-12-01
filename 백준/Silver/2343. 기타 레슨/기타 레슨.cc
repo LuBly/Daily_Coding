fc0843266c1cae5755124015d3bfa6e80c02a07b
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2023-12-01 20:55:13 +0900

    [Silver I] Title: 기타 레슨, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -0,0 +1,46 @@
+//[Baekjoon C++] 기타 레슨 [이분탐색]
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define MAX 100000
+#define INF 987654321
+using namespace std;
+
+int n, m;
+vector<int> arr(MAX);
+//int res = INF;
+int main() {ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int l, r = 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		r += arr[i];	// 최대값 r은 모든 배열의 합
+	}
+	l = *max_element(arr.begin(), arr.begin() + n);	// 최소값 l은 배열의 max값
+
+	while (l <= r) {	
+		int mid = (l + r) / 2;	// 현재 블루레이 길이
+
+		int sum = 0, cnt = 0;	// mid일때 블루레이 수 구하기
+		for (int i = 0; i < n; i++) {
+			if (sum + arr[i] > mid) {
+				sum = 0;
+				cnt++;
+			}
+			sum += arr[i];
+		}
+		if (sum != 0) cnt++;	
+		
+        	// 블루레이 수와 m 비교하여 l,r값 조정하기 
+		if (cnt > m) {
+			l = mid + 1;
+		}
+		else {
+			r = mid - 1;
+		}
+	}
+    
+	cout << l;
+	return 0;
+}
