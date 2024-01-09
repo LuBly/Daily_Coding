8fa4116f7d5dc4ae2630b4b057cc11a9d41cd5ea
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2024-01-09 20:48:26 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 2.79 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -1,71 +1,83 @@
 #include <string>
 #include <vector>
-#include <set>
 #include <queue>
+#include <set>
+#include <algorithm>
+#include <iostream>
 using namespace std;
+/*
+1부터 돌면서 덩어리 체크
+8, 1~3
+7, 4~7
+2, 7~8
 
-// 석탄 덩어리 구조체 
-struct Group{
-    int count;
-    set<int> possibleY;
+1. 맵 전체로 그냥 bfs 한번 싹 돌리기
+-> 돌리면서 지나친 x값들 저장
+2. 각 인덱스에 시추할 수 있는 석유량 저장
+-> 저장해둔 x값에 석유량 +
+3. idx 돌면서 최대값 산출
+*/
+// x좌표별 오일양을 가지고 있을 vector
+vector<int> oilData;
+vector<vector<bool>> isVisit;
+vector<vector<int>> board;
+int length, width;
+int dy[4] = {-1,1,0,0};
+int dx[4] = {0,0,-1,1};
+
+struct Data{
+    int y, x;
 };
 
-int dx[] = {-1,1,0,0};
-int dy[] = {0,0,-1,1};
-int height,width;
-bool visited[500][500];
-vector<vector<int>> map; // 맵
-vector<Group> groups; // 석탄 덩어리 그룹
-
-//반환하는 것은 석탄 덩어리
-Group bfs(int sx, int sy){
-    queue<pair<int,int>> q;
-    set<int> possibleY;
-    q.push({sx,sy});
-    possibleY.insert(sy);
-    visited[sx][sy] = true;
-    int count = 1;
+void bfs(int y, int x){
+    int maxOil = 1;
+    set<int> visitX;
+    visitX.insert(x);
+    
+    queue<Data> q;
+    q.push({y, x});
+    isVisit[y][x] = true;
+    
     while(!q.empty()){
-        int x = q.front().first;
-        int y = q.front().second;
+        int curY = q.front().y;
+        int curX = q.front().x;
         q.pop();
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx < 0 || ny < 0 || nx >= height || ny >= width 
-               || map[nx][ny] == 0 || visited[nx][ny]) continue;
-            visited[nx][ny] = true;
-            q.push({nx,ny});
-            count++;
-            possibleY.insert(ny);
+
+        visitX.insert(curX);
+        
+        for(int k = 0; k < 4; k++){
+            int nextY = curY + dy[k];
+            int nextX = curX + dx[k];
+            
+            if(nextY >= length || nextX >= width || nextY < 0 || nextX < 0) continue;
+            
+            if(board[nextY][nextX] == 1 && !isVisit[nextY][nextX]){
+                q.push({nextY, nextX});
+                isVisit[nextY][nextX] = true;
+                maxOil++;
+            }
         }
     }
-    return {count,possibleY};
-}
-
-void init(vector<vector<int>> land){
-    map = land;
-    height = land.size();
-    width = land[0].size();
     
-    for(int i = 0; i < land.size(); i++){
-        for(int j = 0; j < land[i].size(); j++){
-        	// 석유가 없거나, 이미 방문한 땅이면 패스
-            if(visited[i][j] || land[i][j] == 0) continue;
-            groups.push_back(bfs(i,j));
-        }
+    for(auto iter : visitX){
+        oilData[iter] += maxOil;
     }
 }
 
 int solution(vector<vector<int>> land) {
-    init(land);
-    int answer[501] = {0};
-    int maxNum = 0;
-    for(int i = 0; i < groups.size(); i++){
-        for(auto it : groups[i].possibleY){
-            answer[it] += groups[i].count;
-            maxNum = max(maxNum,answer[it]);
+    board = land;
+    length = land.size();
+    width = land[0].size();
+    oilData.resize(width, 0);
+    isVisit.resize(length, vector<bool>(width, false));
+    
+    for(int y = 0; y < length; y++){
+        for(int x = 0; x < width; x++){
+            if(land[y][x] == 1 && !isVisit[y][x])
+                bfs(y, x);
         }
     }
-    return maxNum;
+    
+    int answer = *max_element(oilData.begin(),oilData.end());
+    return answer;
 }
