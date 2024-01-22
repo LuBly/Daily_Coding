26638ab3f7f9620f134b3bd55dec3881adf594ed
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2024-01-22 17:11:21 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 104 ms, Memory: 14836 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,39 @@
+//[Baekjoon C++] 알고리즘 수업 - 깊이 우선 탐색 2 [DFS] 
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<priority_queue<int>> board;
+vector<int> isVisit;
+int cnt = 1;
+void dfs(int start){
+    isVisit[start] = cnt++;
+    while(!board[start].empty()){
+        int nextNode = board[start].top();
+        board[start].pop();
+        if(!isVisit[nextNode]){
+            dfs(nextNode);
+        }
+    }
+}
+
+int main() {ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int nodeCnt, edgeCnt, startNode;
+    cin >> nodeCnt >> edgeCnt >> startNode;
+
+    board.resize(nodeCnt+1);
+    isVisit.resize(nodeCnt+1, 0);
+    for(int k = 0; k < edgeCnt; k++){
+        int st, dt;
+        cin >> st >> dt;
+        board[st].push(dt);
+        board[dt].push(st);
+    }
+
+    dfs(startNode);
+
+    for(int k = 1; k <= nodeCnt; k++){
+        cout << isVisit[k] << '\n';
+    }
+}
