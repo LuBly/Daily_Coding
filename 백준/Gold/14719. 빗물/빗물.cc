08601d697606bc345f2f71e1e44e563f9e7e7082
Park Hyunho <48556414+LuBly@users.noreply.github.com>
2025-02-03 17:10:15 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -2,10 +2,7 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-// REF : https://hwan-shell.tistory.com/276
-int main() { ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
-    // 한칸 한칸 기준으로
-    // 왼쪽 가장 큰 값, 오른쪽 가장 큰 값 체크
+int main() { ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int h, w;
     cin >> h >> w;
 
@@ -13,14 +10,15 @@ int main() { ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     for(int i = 0; i < w; i++){
         cin >> board[i];
     }
+
+    // 현재 위치를 기준으로 (왼쪽 max, 오른쪽 max 계산 둘 중 작은 값 - 현재 값) 을 +
     int ans = 0;
-    // 현재 기준 왼쪽 최대값, 오른쪽 최대값
-    for(int i = 1; i < w; i++){
-        // 왼쪽
+    for(int i = 0; i < w; i ++){
         int leftMax = -1;
         int rightMax = -1;
         for(int l = 0; l < i; l++) leftMax = max(leftMax, board[l]);
         for(int r = w-1; r > i; r--) rightMax = max(rightMax, board[r]);
+
         ans += max(0, min(leftMax, rightMax) - board[i]);
     }
 
